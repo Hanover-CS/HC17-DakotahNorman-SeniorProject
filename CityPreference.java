449284157cf5f808708f434fbdc7c33551486ad9
normand17 <normand17@hanover.edu>
2017-03-26 21:42:16 -0400

    Final Documentation of CityPreference

--- CityPreference.java
+++ CityPreference.java
@@ -2,6 +2,7 @@ package com.example.dakotahnorman.fishingtextbook;
 
 /**
  * Created by Dakotah Norman on 3/13/2017.
+ * Sets the default city value for the WeatherFragment.
  */
 import android.app.Activity;
 import android.content.SharedPreferences;
@@ -15,7 +16,7 @@ public class CityPreference {
     }
 
     // If the user has not chosen a city yet, return
-    // Sydney as the default city
+    // Hanover as the default city
     String getCity(){
         return prefs.getString("city", "Hanover, US");
     }
